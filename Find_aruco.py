cb989e42fdd208ffae7b08664f75063a0007ef26
SsAaFfIiKk <chf700@yandex.ru>
2020-04-02 14:45:48 +0300

    check aruco id

--- Find_aruco.py
+++ Find_aruco.py
@@ -9,18 +9,23 @@ with h5py.File("parametrs_for_undistort", "r") as f:
     rvecs = f["rvecs"][()]
     tvecs = f["tvecs"][()]
 
+
 def undistort(img, mtx, dist):
     undistorted = cv2.undistort(img, mtx, dist)
     return undistorted
 
 
 class ArucoDetect:
-    def __init__(self):
+    def __init__(self, first_id, second_id):
         self.aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
         self.parameters = aruco.DetectorParameters_create()
         self.corners = None
         self.ids = None
         self.rejectedImgPoints = None
+        self.detect_1 = False
+        self.detect_2 = False
+        self.first_id = first_id
+        self.second_id = second_id
 
     def img_to_gray(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -29,24 +34,54 @@ class ArucoDetect:
     def detect_marker(self, gray):
         self.corners, self.ids, self.rejectedImgPoints = aruco.detectMarkers(gray, self.aruco_dict,
                                                                              parameters=self.parameters)
-
     def draw_markers(self, img):
         frame_markers = aruco.drawDetectedMarkers(img, self.corners, self.ids)
         return frame_markers
 
+    def check_first_id(self):
+        if self.ids is not None:
+            for k in range(0, len(self.ids)):
+                if self.ids[k] == self.first_id:
+                    self.detect_1 = True
+        else:
+            self.detect_1 = False
 
-detect = ArucoDetect()
+    def check_second_id(self):
+        if self.ids is not None:
+            for k in range(0, len(self.ids)):
+                if self.ids[k] == self.second_id:
+                    self.detect_2 = True
+        else:
+            self.detect_2 = False
 
-cap = cv2.VideoCapture(2)
+    def output(self):
+        if self.detect_1 and self.detect_2:
+            print("See both")
+
+        elif self.detect_1:
+            print("See id" + str(self.first_id) + " marker")
+
+        elif self.detect_2:
+            print("See id" + str(self.second_id) + " marker")
+
+        elif self.detect_1 == False and self.detect_2 == False:
+            print("Don't see markers")
+
+
+detect = ArucoDetect(5, 6)
+cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
 
-    undistorted = undistort(frame, mtx, dist)
+    # undistorted = undistort(frame, mtx, dist)
 
-    gray = detect.img_to_gray(undistorted)
+    gray = detect.img_to_gray(frame)
     detect.detect_marker(gray)
-    frame_markers = detect.draw_markers(undistorted)
+    detect.check_first_id()
+    detect.check_second_id()
+    detect.output()
+    frame_markers = detect.draw_markers(frame)
 
     cv2.imshow("frame", frame_markers)
 
